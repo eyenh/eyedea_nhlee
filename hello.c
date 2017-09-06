19d1a4f7da6e57d55748bfbebb85aa397cf85ae0
Lee Na Hyeok <nhlee@eyedea.co.kr>
2017-09-06 11:31:09 +0900

    hello.c modified

--- hello.c
+++ hello.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(void) {
-	puts("Hello World!");
+int main(int argc, char *argv[]) {
+    int i = 0;
+
+    if(argc == 1)
+    	puts("Not enough arguments");
+
+    else {
+    	printf("argc = %d\n", argc);
+
+        for(i=0; i < argc-1; i++)
+		   	printf("%s\n", argv[i+1]);
+    }
 
 	return 0;
-
 }
+
